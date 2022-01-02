4a23f733547f27a9f4dedcc3d212b943d53fb11d
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2022-01-02 10:48:46 +0700

    Make 404 page

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -1,7 +1,17 @@
-export default function error404(){
-  return(
-    <div>
-      <p>Halaman tidak ditemukan!</p>
+import { Link } from "react-router-dom";
+
+export default function error404() {
+  return (
+    <div className="vh-100 d-flex justify-content-center align-items-center text-center fw-normal">
+      <div>
+        <h1 style={{
+          fontSize: '100px',
+        }}>404</h1>
+        <p className="fs-2">Nyari apa bro? halamannya gaada, balik sana.</p>
+        <Link to='/' className="fs-4 text-decoration-none">
+          <p>Kembali ke halaman utama</p>
+        </Link>
+      </div>
     </div>
   )
 }
